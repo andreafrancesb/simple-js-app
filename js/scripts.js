31ed766324bf8f889eccec560924d45243631725
Annie Brown <99762925+andreafrancesb@users.noreply.github.com>
2022-11-26 13:31:53 -0800

    Added Pokemon Objects
    Changed/js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-alert('Hello world');
-
-let favFood = 'Banana';
-document.write(favFood);
+let pokemonList = [
+    { id: 1, name: 'Bulbasaur', hp: 45, height: 0.7, types: ['grass', 'poison']},
+    { id: 2, name: 'Ivysaur', hp: 60, height: 1, types: ['grass', 'poison']},
+    { id: 3, name: 'Venusaur', hp: 80, height: 2, types: ['grass', 'poison']},
+    { id: 4, name: 'Charmander', hp: 39, height: 0.6, types: 'fire'},
+    { id: 5, name: 'Charmeleon', hp: 58, height: 1.1, types: 'fire'},
+    { id: 6, name: 'Charizard', hp: 78, height: 1.7, types: ['fire', 'flying']},
+    { id: 7, name: 'Squirtle', hp: 44, height: 0.5, types: 'water'},
+    
+  ];
