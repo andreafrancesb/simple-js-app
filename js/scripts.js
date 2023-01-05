c65444ade9e882a4e322e49f9204b749e1f062a9
Annie Brown <99762925+andreafrancesb@users.noreply.github.com>
2023-01-04 22:56:27 -0800

    Added Loops and Styling
    Changed/js/scripts.js
    Changed/index.html
    Changed/css/style.css
    Added/images/favicon.png
    Added/images/Pokedex.png

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+/* Pokemon list and attributes */
+
 let pokemonList = [
     { id: 1, name: 'Bulbasaur', hp: 45, height: 0.7, types: ['grass', 'poison']},
     { id: 2, name: 'Ivysaur', hp: 60, height: 1, types: ['grass', 'poison']},
@@ -6,5 +8,19 @@ let pokemonList = [
     { id: 5, name: 'Charmeleon', hp: 58, height: 1.1, types: ['fire']},
     { id: 6, name: 'Charizard', hp: 78, height: 1.7, types: ['fire', 'flying']},
     { id: 7, name: 'Squirtle', hp: 44, height: 0.5, types: ['water']},
-    
-  ];
+]
+
+/* Loop to display pokemon and highlight sizes */
+
+    for (let i = 0;
+      i < pokemonList.length; i++) {
+      if(pokemonList[i].height >= 2) {
+          document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - Woah, that is a big pokemon!" + "<br>")
+      }
+      else if (pokemonList[i].height >= 2 && pokemonList[i].height < 5){
+          document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - This is a medium pokemon." + "<br>")
+      }
+      else {
+          document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - This is a small pokemon." + "<br>")
+      }
+  }
